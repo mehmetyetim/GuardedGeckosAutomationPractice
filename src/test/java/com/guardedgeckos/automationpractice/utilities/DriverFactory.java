9dd4dce11c1edb5a0ccc1667a4ca15e6a51d3dd5
mehmetyetim <98969298+mehmetyetim@users.noreply.github.com>
2022-03-07 15:28:16 +0000

    pom updated, DriverFactory completed, Hooks updated

--- src/test/java/com/guardedgeckos/automationpractice/utilities/DriverFactory.java
+++ src/test/java/com/guardedgeckos/automationpractice/utilities/DriverFactory.java
@@ -1,4 +1,100 @@
 package com.guardedgeckos.automationpractice.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DriverFactory {
+
+    private static Map<String, String> mobileEmulation = new HashMap<>();
+    private static ChromeOptions chromeOptions = new ChromeOptions();
+
+    private DriverFactory() {
+
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver get() {
+        // Test
+        if (driver == null) {
+            // this line will tell which browser should open based on the value from properties file
+            String browser = ConfigurationReader.get("browser");
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+                case "ie":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                case "edge":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari":
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac"))
+                        throw new WebDriverException("Your OS doesn't support Safari");
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+
+                case "chrome-nexus-5":
+                    WebDriverManager.chromedriver().setup();
+                    mobileEmulation.put("deviceName","Nexus 5");
+                    chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+
+                case "chrome-galaxy-s5":
+                    WebDriverManager.chromedriver().setup();
+                    mobileEmulation.put("deviceName","Galaxy S5");
+                    chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+            }
+
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+            chromeOptions = null;
+            mobileEmulation = null;
+        }
+    }
+
 }
