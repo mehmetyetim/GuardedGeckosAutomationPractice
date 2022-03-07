34a172a367307108b47eaab42debae317708e701
andymurray32 <andrewmurray5@outlook.com>
2022-03-07 15:49:59 +0000

    Created HomePage Class

---
+++ src/test/java/com/guardedgeckos/automationpractice/pages/IHopeYouGuysAreHappy.java
@@ -0,0 +1,10 @@
+package com.guardedgeckos.automationpractice.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class IHopeYouGuysAreHappy extends BasePage{
+    protected static WebDriver driver;
+    protected static String URL;
+
+
+}
