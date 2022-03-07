f69b924ca1b5dbb8ab5efa35968ae35b7213a923
BwongmcST <ben.manchunwong@gmail.com>
2022-03-07 22:45:59 +0000

    Update OrderHistoryPage.java
    add more methods in order history page

--- src/test/java/com/guardedgeckos/automationpractice/pages/OrderHistoryPage.java
+++ src/test/java/com/guardedgeckos/automationpractice/pages/OrderHistoryPage.java
@@ -1,6 +1,7 @@
 package com.guardedgeckos.automationpractice.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
@@ -15,6 +16,17 @@ public class OrderHistoryPage {
     private By orderDateColumn = new By.ByClassName("history_date");
     private By orderPriceColumn = new By.ByClassName("price");
     private By orderStatusColumn = new By.ByClassName("label");
+    private By orderHistoryDetailColumn = new By.ByClassName("history_detail");
+    private By plusButton = new By.ByClassName("footable-toggle");
+
+    private By buttonLink = new By.ByTagName("a");
+    private By linkButton = new By.ByClassName("link-button");
+    private By button = new By.ByClassName("btn");
+    private By myAccountPageFooter = new By.ByClassName("footer_links");
+
+    //My account page footer buttons
+    private final int backToYourAccountButton = 0;
+    private final int homeButton = 1;
 
 //    public OrderHistoryPage(WebDriver webDriver){
 //    }
@@ -121,6 +133,79 @@ public class OrderHistoryPage {
         return output;
     }
 
+    public void clickOrderInvoiceButton(int orderIndex){                                      //Can't see this web element if the browser window is too small!
+        try {
+                    webDriver.findElement(orderHistoryTable)
+                            .findElement(orderHistoryList)
+                            .findElements(orderTableRow)
+                            .get(orderIndex)
+                            .findElement(linkButton)
+                            .click();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("ALERT: INVALID INDEX");
+        }
+    }
 
+    public void clickOrderDetailsButton(int orderIndex){                                      //Can't see this web element if the browser window is too small!
+        try {
+            webDriver.findElement(orderHistoryTable)
+                    .findElement(orderHistoryList)
+                    .findElements(orderTableRow)
+                    .get(orderIndex)
+                    .findElement(orderHistoryDetailColumn)
+                    .findElement(button)
+                    .click();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("ALERT: INVALID INDEX");
+        }
+    }
+
+    public void clickReorderButtonFromTable(int orderIndex){                                     //Can't see this web element if the browser window is too small!
+        try {
+            webDriver.findElement(orderHistoryTable)
+                    .findElement(orderHistoryList)
+                    .findElements(orderTableRow)
+                    .get(orderIndex)
+                    .findElement(orderHistoryDetailColumn)
+                    .findElement(linkButton)
+                    .click();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("ALERT: INVALID INDEX");
+        }
+    }
+
+    public void clickPlusButton(int orderIndex){                                                 //Can't see this web element if the browser window is too Large
+        try {
+            webDriver.findElement(orderHistoryTable)
+                    .findElement(orderHistoryList)
+                    .findElements(orderTableRow)
+                    .get(orderIndex)
+                    .findElement(plusButton)
+                    .click();
+        }catch (IndexOutOfBoundsException| ElementNotInteractableException e){
+            System.out.println("ALERT: INVALID INDEX OR ELEMENT NOT INTERACTABLE");
+        }
+    }
+
+    public void clickPDFButtonInFootableBox(int orderIndex){                                                 //Can't see this web element if the browser window is too Large
+        try {
+            webDriver.findElement(orderHistoryTable)
+                    .findElement(orderHistoryList)
+                    .findElements(orderTableRow)
+                    .get(orderIndex+1)
+                    .findElement(plusButton)
+                    .click();
+        }catch (IndexOutOfBoundsException| ElementNotInteractableException e){
+            System.out.println("ALERT: INVALID INDEX OR ELEMENT NOT INTERACTABLE");
+        }
+    }
+
+
+    public void clickHomeButton(){
+        webDriver.findElement(myAccountPageFooter).findElements(buttonLink).get(homeButton).click();
+    }
+    public void clickBackToYourAccountButton(){
+        webDriver.findElement(myAccountPageFooter).findElements(buttonLink).get(backToYourAccountButton).click();
+    }
 
 }
