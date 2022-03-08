f873af40e4bd908a979008db0aa6c92a5c7b446f
mehmetyetim <98969298+mehmetyetim@users.noreply.github.com>
2022-03-08 23:25:43 +0000

    HomePage more methods and fields added

--- src/test/java/com/guardedgeckos/automationpractice/pages/HomePage.java
+++ src/test/java/com/guardedgeckos/automationpractice/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,24 +13,30 @@ import java.util.List;
 
 public class HomePage extends BasePage {
 
+    protected static final String DEFAULT_URL = "http://automationpractice.com/";
+
     private final By nextArrowOnImage = By.className("bx-next");
     private final By prevArrowOnImage = By.className("bx-prev");
     private final By shopNowButton = By.xpath("//button[@type='button']");
 
     // it locates all 7 images on home page except the biggest
     private final By allImagesOnHomePageExceptTheBiggest = By.xpath("//img[@class='item-img']");
-    private final By textOnBiggestFirstImage = By.xpath("//a[@title='sample-1']/following-sibling::div/p[1]");
-    private final By titleOnBiggestSecondImage = By.xpath("//a[@title='sample-2']/following-sibling::div/h2");
-    private final By biggestImageSection = By.id("homeslider");
-    private final By secondBiggestImage = By.xpath("//img[@alt='sample-2']");
+    private final By textOnFirstMovingImage = By.xpath("//a[@title='sample-1']/following-sibling::div/p[1]");
+    private final By titleOnSecondMovingImage = By.xpath("//a[@title='sample-2']/following-sibling::div/h2");
+    private final By movingImagesSection = By.id("homeslider");
+    private final By firstMovingImage = By.xpath("(//img[@alt='sample-1'])[2]");
+    private final By secondMovingImage = By.xpath("//img[@alt='sample-2']");
+    private final By thirdMovingImage = By.xpath("(//img[@alt='sample-3'])[2]");
+    private final By priceOnFirstProduct = By.xpath("(//span[@itemprop='price'])[1]");
+    private final By firstProduct = By.xpath("(//div[@class='product-container'])[1]");
 
-    private final WebDriver driver;
     private final WebDriverWait wait;
+    Actions action;
 
     public HomePage(WebDriver driver) {
-        super(driver);
-        this.driver = driver;
+        super(driver, DEFAULT_URL);
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        action = new Actions(driver);
     }
 
     public void goToNextImage() {
@@ -38,6 +45,28 @@ public class HomePage extends BasePage {
 
     public void goToPreviousImage() {
         driver.findElement(prevArrowOnImage).click();
+    }
+
+    public boolean goToNextImageAndCheckIfTheNextImageDisplayed() {
+        goToNextImage();
+        return driver.findElement(secondMovingImage).isDisplayed();
+    }
+
+    public boolean isThirdMovingImageDisplayedAfterAWhile(){
+        wait.until(ExpectedConditions.elementToBeClickable(thirdMovingImage));
+        return driver.findElement(thirdMovingImage).isDisplayed();
+    }
+
+    public boolean isThirdMovingImageDisplayed(){
+        return driver.findElement(thirdMovingImage).isDisplayed();
+    }
+
+    public boolean isPriceOnFirstProductDisplayed(){
+        return driver.findElement(priceOnFirstProduct).isDisplayed();
+    }
+
+    public void hoverOverFirstProduct(){
+        action.moveToElement(driver.findElement(firstProduct)).perform();
     }
 
     //region small7images
