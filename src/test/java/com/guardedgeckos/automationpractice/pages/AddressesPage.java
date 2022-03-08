7845676b9b0f185d59900b291b754cdcac025e26
BwongmcST <ben.manchunwong@gmail.com>
2022-03-08 02:25:12 +0000

    Update AddressesPage.java
    add methods

--- src/test/java/com/guardedgeckos/automationpractice/pages/AddressesPage.java
+++ src/test/java/com/guardedgeckos/automationpractice/pages/AddressesPage.java
@@ -9,7 +9,19 @@ public class AddressesPage {
 
     private By centerColumn = new By.ById("center_column");
 
+    private By addressField = new By.ByClassName("addresses");
+    private By addressFieldRow = new By.ByClassName("bloc_adresses");               // row number
+    private By addressFieldColumn = new By.ByClassName("col-xs-12");                // 2 blocks (index 0-1) in a row
+
+    private By addressFieldAlias = new By.ByClassName("page-subheading");
+    private By addressFieldCompanyName = new By.ByClassName("address_company");
+    private By addressFieldAddress1 = new By.ByClassName("address_address1");
+    private By addressFieldAddress2 = new By.ByClassName("address_address2");
+    private By addressFieldPhoneNum = new By.ByClassName("address_phone");
+    private By addressFieldMobilePhoneNum = new By.ByClassName("address_phone_mobile");
+
     //General
+    private By list = new By.ByTagName("li");
     private By clearfix = new By.ByClassName("clearfix");
     private By options = new By.ByTagName("option");
     private By colorMyAccountText = new By.ByClassName("color-myaccount");
@@ -19,8 +31,6 @@ public class AddressesPage {
     private By linkButton = new By.ByClassName("link-button");
     private By button = new By.ByClassName("btn");
     private By myAccountPageFooter = new By.ByClassName("footer_links");
-    private By footableRow = new By.ByClassName("footable-row-detail-row");
-    private By foottableRowValue = new By.ByClassName("footable-row-detail-value");
 
     //My account page footer buttons
     private final int backToYourAccountButton = 0;
@@ -37,5 +47,145 @@ public class AddressesPage {
                 .findElement(button)
                 .click();
     }
+
+    public String getAddressAlias(int rowIndex, int columnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(rowIndex)
+                    .findElements(addressFieldColumn).get(columnIndex)
+                    .findElement(addressFieldAlias).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public String getAddressCompanyName(int rowIndex, int columnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(rowIndex)
+                    .findElements(addressFieldColumn).get(columnIndex)
+                    .findElement(addressFieldCompanyName).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public String getAddressAddressOne(int rowIndex, int columnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(rowIndex)
+                    .findElements(addressFieldColumn).get(columnIndex)
+                    .findElement(addressFieldAddress1).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public String getAddressAddressTwo(int rowIndex, int columnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(rowIndex)
+                    .findElements(addressFieldColumn).get(columnIndex)
+                    .findElement(addressFieldAddress2).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public String getAddressPhoneNum(int blockRowIndex, int blockColumnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(blockRowIndex)
+                    .findElements(addressFieldColumn).get(blockColumnIndex)
+                    .findElement(addressFieldPhoneNum).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public String getAddressMobilePhoneNum(int blockRowIndex, int blockColumnIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(blockRowIndex)
+                    .findElements(addressFieldColumn).get(blockColumnIndex)
+                    .findElement(addressFieldMobilePhoneNum).getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+
+    public void updateAddressBlock(int blockRowIndex, int blockColumnIndex){
+        try {
+            webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(blockRowIndex)
+                    .findElements(addressFieldColumn).get(blockColumnIndex)
+                    .findElements(button).get(0)
+                    .click();
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+        }
+    }
+
+    public void deleteAddressBlock(int blockRowIndex, int blockColumnIndex, boolean areYouSure){
+        try {
+            webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(blockRowIndex)
+                    .findElements(addressFieldColumn).get(blockColumnIndex)
+                    .findElements(button).get(1)
+                    .click();
+
+            if(areYouSure){
+             webDriver.switchTo().alert().accept();
+            }else {
+                webDriver.switchTo().alert().dismiss();
+            }
+
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+        }
+    }
+
+    //Another way to access string in each block
+    //City, State, Postal code & Country only accessible by inputting row number, this website is trolling
+
+    public String getStringInAddressBlock(int blockRowIndex, int blockColumnIndex, int rowIndex){
+        try {
+            return webDriver
+                    .findElement(addressField)
+                    .findElements(addressFieldRow).get(blockRowIndex)
+                    .findElements(addressFieldColumn).get(blockColumnIndex)
+                    .findElements(list).get(rowIndex)
+                    .getText();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("INVALID INDEX");
+            return "INVALID INDEX";
+        }
+    }
+
+    public void clickHomeButton(){
+        webDriver.findElement(myAccountPageFooter).findElements(buttonLink).get(homeButton).click();
+    }
+    public void clickBackToYourAccountButton(){
+        webDriver.findElement(myAccountPageFooter).findElements(buttonLink).get(backToYourAccountButton).click();
+    }
+
+
+
 
 }
