f55e12f1b8965158edb081984c8edc4a6bc18df3
Sajad <osajad_123@hotmail.co.uk>
2022-03-08 10:23:56 +0000

    Minor Update

--- src/test/java/com/guardedgeckos/automationpractice/step_definitions/LogInStepDefs.java
+++ src/test/java/com/guardedgeckos/automationpractice/step_definitions/LogInStepDefs.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class SignInStepDefs {
+public class LogInStepDefs {
         private static WebDriver webDriver;
 
         @Before
